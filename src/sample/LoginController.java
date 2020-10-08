c816ab4bdaaef0223da1d2455dcc945dae25a1e3
Sabrina <labradorsabrina@gmail.com>
2020-10-08 12:22:33 -0400

    Today commit

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,17 +1,56 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
+import java.io.File;
+import java.util.ResourceBundle;
 
-public class LoginController {
+import java.net.URL;
+
+
+public class LoginController implements Initializable {
 
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView logoImageView;
+    @FXML
+    private ImageView lockImageView;
 
-    public void cancelButtonOnAction(ActionEvent Event){
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("/img/Friendship.gif");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File logoFile = new File("/img/Image2-01.png");
+        Image logoImage = new Image(logoFile.toURI().toString());
+        logoImageView.setImage(logoImage);
+
+        File lockFile = new File("/img/pendulum.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+
+    }
+
+    public void loginButtonOnAction(ActionEvent event){
+        loginMessageLabel.setText("You tried to login");
+    }
+
+
+
+    public void cancelButtonOnAction(ActionEvent event){
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
